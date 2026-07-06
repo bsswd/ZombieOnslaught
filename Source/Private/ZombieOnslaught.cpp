972bb44062683f7027fe64928362c00e9aa129fc
Alexander Sinkin <alexandersinkin@mail.ru>
2026-07-06 12:46:43 +0700

    solution

--- Source/Private/ZombieOnslaught.cpp
+++ Source/Private/ZombieOnslaught.cpp
@@ -9,9 +9,12 @@
 
 int main(int argc, char* argv[])
 {
+    int solution(int number);
+    
+    
     //OneToOneBattle firstBattle;
     //DefenceLogic firstDefenceLogic;
-    BoardGame boardGameSession;
+    //BoardGame boardGameSession;
     
     //Fighter fighter1("Lew", 10, 2);
     //Fighter fighter2("Harry", 5, 4);
@@ -19,13 +22,36 @@ int main(int argc, char* argv[])
     //std::string firstDefenceResult = firstDefenceLogic.DefenseResult(10, 20.f, 5);
     //std::string firstBattleWinner = firstBattle.DeclareWinner(&fighter1, &fighter2, "Lew");
     
-    char boardGameSessionResult = boardGameSession.fightResolve('k', 'A');
+    //char boardGameSessionResult = boardGameSession.fightResolve('k', 'A');
     
     //std::cout << firstDefenceResult << std::endl;
     //std::cout << firstBattleWinner << std::endl;
-    std::cout << boardGameSessionResult << std::endl;
+    //std::cout << boardGameSessionResult << std::endl;
+    
+    std::cout << solution(20) << std::endl;
+    
+    
     
     return 0;
+}
+
+int solution(int number)
+{
+    int result = 0;    
+    if (number <= 0) return 0;
+    
+    for (int i = number - 1; i > 0; i--)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            result += i;
+            continue;
+        }
+        
+        if (i % 3 == 0 || i % 5 == 0) result += i;
+    }
+   
+    return result;
 }
 
 
